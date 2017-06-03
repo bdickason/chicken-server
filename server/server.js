3e0a211184136b8a994a14ebce836f920c494e62
Brad Dickason <brad.dickason@oculus.com>
2017-06-02 19:00:14 -0700

    added hack for privacy poligy

--- server/server.js
+++ server/server.js
@@ -8,6 +8,15 @@ const api = require('./api')
 app.use(bodyParser.json())
 app.use('/api', api)
 
+// Hack to get privacy policy up asap
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/privacy', function(req, res) {
+  res.render('privacy')
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
